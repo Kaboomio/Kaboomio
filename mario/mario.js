a8568f0076cc758c66f35bda49a155647654b036
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-11 10:15:02 -0700

    loaded sprites in mario.js for start screen and first level

--- mario/mario.js
+++ mario/mario.js
@@ -1,8 +1,29 @@
 import kaboom from "../kaboom/dist/kaboom.mjs";
 
-kaboom();
+kaboom({
+    global: true,
+    // width: 600,
+    // height: 300,
+    fullscreen: true,
+    scale: 2, 
+    debug: true,
+    background: [0, 0, 0, 1],
+});
 
 add([
     text("hello"),
     pos(120, 80),
 ]);
+
+//mario level sprites
+loadSprite('coin', '../assets/coin.png');
+loadSprite('brick', '../assets/brick.png');
+loadSprite('block', '../assets/box.png');
+loadSprite('mario', '../assets/mario.png');
+loadSprite('mushroom', '../assets/mushroom.png');
+loadSprite('evil-mushroom', '../assets/evil-mushroom.png');
+loadSprite('surprise-box', '../assets/surprise-box.png');
+loadSprite('pipe', '../assets/pipe.png');
+
+//start screen sprites
+loadSprite('start-screen', '../assets/start-screen.png');
