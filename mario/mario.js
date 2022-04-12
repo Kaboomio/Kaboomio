6554ba51f2a19c4f42b72020a1df80c779e60501
Cody-Wise <cody@sharptackmedia.com>
2022-04-12 16:14:24 -0700

    Added Mario fall to death and gameover sound

--- mario/mario.js
+++ mario/mario.js
@@ -42,7 +42,9 @@ loadSound('gameOver', 'gameOver.mp3');
 loadSound('powerUp', 'powerUp.mp3');
 loadSound('pipeSound', 'pipe.mp3');
 
+const fallToDeath = 500;
 
+const music = play('theme'); 
 
 
 
@@ -65,6 +67,7 @@ scene('start', () => {
 
     onKeyDown('space', () => {
         go('game', { score: 0, count: 0 });
+        
     });
     
     onUpdate(() => {
@@ -72,14 +75,11 @@ scene('start', () => {
     });
 });
 
-const music = play('theme');
-
-    
 //GAME SCENE
 scene('game', ({ score, count }) => {
     layers(['bg', 'obj', 'ui'], 'obj');
 
-    music.play();
+    
     
 
     add([
@@ -97,7 +97,7 @@ scene('game', ({ score, count }) => {
         sprite('mario'), 
         solid(), 
         area({ width: 20, height: 20 }),
-        pos(36, 0),
+        pos(36, 0),        
         body(),
         origin('bot'),
         'mario'
@@ -139,7 +139,17 @@ scene('game', ({ score, count }) => {
         } else {
             isJumping = true;
         }
+        music.play();
+        
     });
+
+    mario.onUpdate(() => {
+        camPos(mario.pos);
+        if (mario.pos.y >= fallToDeath) {
+            go('lose', { score: scoreLabel.value, time: timeLeft, level: currentLevel });
+        }
+    });
+
 
     let fireballDirection = 'down';
 
@@ -377,7 +387,10 @@ scene('game', ({ score, count }) => {
 
 });
 
+
 scene('lose', ({ score, time, level }) => {
+    const gameOverMusic = play('gameOver');
+
     add([
         text('Game Over', {
             size: 226,
@@ -385,7 +398,8 @@ scene('lose', ({ score, time, level }) => {
         origin('center'), 
         pos(480, 125),
         scale(0.25),
-        music.pause()
+        music.pause(),
+        gameOverMusic.play()
     ]);
     add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)]);
 
@@ -436,11 +450,7 @@ scene('lose', ({ score, time, level }) => {
     });
 
     onKeyPress('enter', async () => {
-<<<<<<< HEAD
-        await createScore(score, level, n.value, time);
-=======
         await createScore(score, level, n.value, time / 60);
->>>>>>> 1a844b8fe0f3ca50867412a30f6639ab19cc04cc
         location.replace('../home-page');
     });
 });
@@ -478,8 +488,6 @@ function patrol(distance = 150, speed = 50, dir = 1) {
         },
     };
 }
-
-let main = document.querySelector('main');
 
 
 // function renderAndAppendForm(){
