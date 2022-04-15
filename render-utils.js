1bb7ded06219774ba1bd56332bb95111feb1491f
Cody-Wise <cody@sharptackmedia.com>
2022-04-14 23:25:20 -0700

    Small change on profile page

--- render-utils.js
+++ render-utils.js
@@ -328,7 +328,7 @@ export function renderAboutHeader(profile) {
     logoutTooltip.textContent = 'Logout';
     logoutTooltip.classList.add('tooltip');
     
-    // CONDITIONAL ELEMENT ATTRIBUTES IF USER HAS NOT FINISHED PROFILE SETU
+    // CONDITIONAL ELEMENT ATTRIBUTES IF USER HAS NOT FINISHED PROFILE SETUP
     if (!profile.username) {
         nameEl.textContent = 'Dont forget to finish profile setup!';
         settingsImg.style.animation = 'pulse 2s infinite ease-in-out';
